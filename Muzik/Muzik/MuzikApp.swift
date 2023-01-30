eb9b2b3624d2dd562346a9a70585f91dc4ca7156
shayan <syed.shayan18@gmail.com>
2023-01-30 14:47:29 +0500

    Setup initial project files
    - Project setup with SwiftUI + Tests

---
+++ Muzik/Muzik/MuzikApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuzikApp.swift
+//  Muzik
+//
+//  Created by Shayan Ali on 30.01.23.
+//
+
+import SwiftUI
+
+@main
+struct MuzikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Muzik/MuzikTests/MuzikTests.swift
@@ -0,0 +1,12 @@
+//
+//  MuzikTests.swift
+//  MuzikTests
+//
+//  Created by Shayan Ali on 30.01.23.
+//
+
+import XCTest
+@testable import Muzik
+
+final class MuzikTests: XCTestCase {
+}
